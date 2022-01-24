7d9f826c729996714ff6c483b9ef394f032416e0
Alex Daniel <alex.adar93@gmail.com>
2022-01-23 20:38:40 -0500

    Agregando una carita feliz en el alert.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let advertencia = "Esto recien se esta elaborando, a medida que tengo tiempo lo estoy realizando prometo terminarlo pronto!!!  =)"
-let msj1 = "Paciencia porfavor =)"
+let msj1 = "Paciencia porfavor =) =) "
 
 alert(advertencia)
 alert(msj1)
