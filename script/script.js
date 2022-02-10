813b5b9207d83291534ffb0e89208a149354b15e
Alex Daniel <alex.adar93@gmail.com>
2022-02-10 12:36:56 -0500

    Cambiando el diseño varias cosas -> empezando responsive desde cero

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
-let advertencia = "Poco a poco avanzando =) falta el diseño responsive y algunos enlaces!"
-let msj1 = "Paciencia porfavor =) =) "
+const menu_bar = document.querySelector('.menu-bar')
+const menu_nav = document.querySelector('.menu-nav')
 
+menu_bar.addEventListener('click',()=>{
+    menu_nav.classList.toggle('mostrar')
+})
+
+
+let advertencia = 'Algunos enlaces aun faltan terminar pero el responsivo ya quedo, fue cansado pero ahi queda creo!!'
 alert(advertencia)
-alert(msj1)
-
-
